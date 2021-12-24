1844f4d18ba297e7d13dc7431d972eec56b9eae9
Dera Destri Herisma <96503396+DeraDestriHerisma@users.noreply.github.com>
2021-12-24 08:56:20 +0700

    Add files via upload

---
+++ comment.dart
@@ -0,0 +1,12 @@
+/// This is main function
+/// Will executed by dart
+void main(){
+  // This is variable
+  var name = 'Dera Destri Herisma';
+  
+  /**
+   * This is multi-line comment
+   * you can add more here
+   */
+  print(name);
+}
---
+++ variable.dart
@@ -0,0 +1,36 @@
+/// this is main function
+/// will executed by dart
+void main() {
+  // this is variable
+  var name = 'dera destri herisma'; 
+  // untuk menampilkan hasil dari variable name
+  print(name);
+  print(name);
+  print(name);
+  print(name);
+
+  var firstname = 'dera';
+  final lastname = 'herisma'; // this is final
+
+  firstname = 'tono'; 
+
+  print(firstname);
+  print(lastname);
+
+  final array1 = [1,2,3];
+  const array2 = [1,2,3];
+
+  array1[0] = 5;
+
+  print(array1);
+  print(array2);
+
+  late var value = getValue();
+  print('variabel sudah dibuat');
+  print(value);
+}
+
+String getValue(){
+  print('getValue() dipanggil');
+  return 'dera destri herisma';
+}
